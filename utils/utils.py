328667f9dcf0fe7593992b6c8df40155abeffe08
teachertian <380869212@qq.com>
2019-02-11 15:53:29 -0500

    [utils] Put the function "string_to_bool" in the right place.

--- utils/utils.py
+++ utils/utils.py
@@ -4,3 +4,10 @@ from datetime import datetime
 def get_timestamp(t=None):
     dt = t if t else datetime.utcnow()
     return dt.isoformat() + 'Z'
+
+
+def str_to_boolean(string):
+    if string == "True":
+        return True
+    else:
+        return False
