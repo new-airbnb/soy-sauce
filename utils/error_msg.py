1a95c55b95394337abc5299907de3010dc5c1342
teachertian <380869212@qq.com>
2019-02-11 15:41:14 -0500

    [error_msg] modify the contents of WRONG_PASSWORD_OR_EMAIL in order to unify the error message style.

--- utils/error_msg.py
+++ utils/error_msg.py
@@ -4,4 +4,4 @@ MSG_400 = 'Missing or illegal argument'
 # Custom Message
 ILLEGAL_ARGUMENT = MSG_400
 DUPLICATE_EMAIL = 'User with the same email exists'
-WRONG_PASSWORD_OR_EMAIL_ADDRESS = 'invalid email address or password'
+WRONG_PASSWORD_OR_EMAIL_ADDRESS = 'Invalid email address or password'
