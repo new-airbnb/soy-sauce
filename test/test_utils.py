c6c77585142645a363a690cbfc69a0880dc0a1c5
jqzhang <zhangjiangqige@gmail.com>
2019-02-13 13:04:13 -0500

    integrate Travis and preliminary tests

---
+++ test/test_utils.py
@@ -0,0 +1,15 @@
+from utils import utils
+
+
+class TestUtils(object):
+    def test_str_to_boolean(self):
+        cases = [
+            ("True", True),
+            ("False", False)
+        ]
+        for s, gt in cases:
+            assert utils.str_to_boolean(s) == gt
+
+    def test_get_timestamp(self):
+        stamp = utils.get_timestamp()
+        assert stamp.endswith('Z')
